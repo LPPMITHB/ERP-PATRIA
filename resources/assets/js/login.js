2849704cd24616ef21e0c504f1b4fcca8900edae
Yonathan <mynameyoyoo@gmail.com>
2018-11-18 23:19:52 +0700

    apus dulu

--- resources/assets/js/login.js
+++
@@ -1,4 +0,0 @@
-window.$ = window.jQuery = require('jquery');
-
-require('admin-lte/bower_components/bootstrap/dist/js/bootstrap.min.js');
-require('admin-lte/plugins/iCheck/icheck.min.js');
--- resources/views/includes/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="pull-right hidden-xs">
-    <b>Version</b> Alpha 0.1
-</div>
-<strong>Copyright &copy; {{ date('Y') }}</strong> LPPM ITHB.
