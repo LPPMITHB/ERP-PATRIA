2898b6896394e7f968de7c1589d0a3c1ecae5e0c
ChristianSantosa <christiansentosa11@gmail.com>
2018-12-14 01:07:37 +0700

    bill of services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     protected $table = 'mst_service';
+
+    public function branch()
+    {
+        return $this->belongsTo('App\Models\Branch');
+    }
+
+    public function boss()
+    {
+        return $this->hasMany('App\Models\Bos');
+    }
+
+    public function bosDetails()
+    {
+        return $this->hasMany('App\Models\BosDetail');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Branch');
+    }
     
 }
