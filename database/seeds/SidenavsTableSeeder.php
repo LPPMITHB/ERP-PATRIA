b515f069498bfa71c0d782021f329809557ee535
ChristianSantosa <christiansentosa11@gmail.com>
2019-01-07 17:14:48 +0700

    perbaikan material write off, migration WR

--- database/seeds/SidenavsTableSeeder.php
+++ database/seeds/SidenavsTableSeeder.php
@@ -407,6 +407,24 @@ class SidenavsTableSeeder extends Seeder
             'route_name' => 'rap_repair.showMaterialEvaluation',
         ]);
 
+        $createWr = Menu::where('route_name','work_request.create')->select('id')->first()->id;
+        DB::table('sidenav')->insert([
+            'menu_id' => $createWr,
+            'route_name' => 'work_request.create',
+        ]);
+
+        $indexApproveWR = Menu::where('route_name','work_request.indexApprove')->select('id')->first()->id;
+        DB::table('sidenav')->insert([
+            'menu_id' => $indexApproveWR,
+            'route_name' => 'work_request.indexApprove',
+        ]);
+
+        $viewWr = Menu::where('route_name','work_request.index')->select('id')->first()->id;
+        DB::table('sidenav')->insert([
+            'menu_id' => $viewWr,
+            'route_name' => 'work_request.index',
+        ]);
+
         $createPr = Menu::where('route_name','purchase_requisition.create')->select('id')->first()->id;
         DB::table('sidenav')->insert([
             'menu_id' => $createPr,
