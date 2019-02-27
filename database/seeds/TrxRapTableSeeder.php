6cca74d912ffd0ec125c5ff37d05c58b0e091389
Nathan <nathanaeltimotius@gmail.com>
2019-02-27 16:11:11 +0700

    Update Seeder BOM, BOM/BOS, PR, PO

--- database/seeds/TrxRapTableSeeder.php
+++ database/seeds/TrxRapTableSeeder.php
@@ -13,7 +13,7 @@ class TrxRapTableSeeder extends Seeder
     public function run()
     {
         
-        
+
         \DB::table('trx_rap')->delete();
         
         \DB::table('trx_rap')->insert(array (
@@ -23,72 +23,24 @@ class TrxRapTableSeeder extends Seeder
                 'number' => 'RAP-1900001',
                 'project_id' => 1,
                 'bom_id' => 1,
-                'total_price' => 117600000,
+                'total_price' => 117600000.0,
                 'user_id' => 1,
                 'branch_id' => 1,
                 'created_at' => '2019-01-15 09:56:21',
                 'updated_at' => '2019-01-15 09:56:22',
-            ), /*
+            ),
             1 => 
             array (
                 'id' => 2,
                 'number' => 'RAP-1900002',
-                'project_id' => 1,
-                'bom_id' => 2,
-                'total_price' => 203400000,
-                'user_id' => 1,
-                'branch_id' => 1,
-                'created_at' => '2019-01-15 09:56:50',
-                'updated_at' => '2019-01-15 09:56:51',
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'number' => 'RAP-1900003',
-                'project_id' => 1,
-                'bom_id' => 3,
-                'total_price' => 237270000,
-                'user_id' => 1,
-                'branch_id' => 1,
-                'created_at' => '2019-01-15 09:57:19',
-                'updated_at' => '2019-01-15 09:57:19',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'number' => 'RAP-1900004',
                 'project_id' => 2,
                 'bom_id' => 4,
-                'total_price' => '3113250000',
+                'total_price' => 0.0,
                 'user_id' => 1,
                 'branch_id' => 1,
-                'created_at' => '2019-01-15 09:57:54',
-                'updated_at' => '2019-01-15 09:57:54',
+                'created_at' => '2019-02-27 14:53:58',
+                'updated_at' => '2019-02-27 14:53:59',
             ),
-            4 => 
-            array (
-                'id' => 5,
-                'number' => 'RAP-1900005',
-                'project_id' => 2,
-                'bom_id' => 5,
-                'total_price' => 24000,
-                'user_id' => 1,
-                'branch_id' => 1,
-                'created_at' => '2019-01-15 09:58:20',
-                'updated_at' => '2019-01-15 09:58:20',
-            ),
-            5 => 
-            array (
-                'id' => 6,
-                'number' => 'RAP-1900006',
-                'project_id' => 2,
-                'bom_id' => 6,
-                'total_price' => 74262000,
-                'user_id' => 1,
-                'branch_id' => 1,
-                'created_at' => '2019-01-15 09:58:45',
-                'updated_at' => '2019-01-15 09:58:46',
-            ), */
         ));
         
         
