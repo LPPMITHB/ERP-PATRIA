5240a2193c9b5b16e6e608822cf1944944336890
mynameyoyoo <mynameyoyoo@gmail.com>
2019-03-19 16:39:51 +0700

    create PR job order

--- resources/views/branch/show.blade.php
+++ resources/views/branch/show.blade.php
@@ -91,3 +91,11 @@
 </div>
 
 @endsection
+
+@push('script')
+<script>
+     $(document).ready(function(){
+        $('div.overlay').hide();
+    });
+</script>
+@endpush
--- resources/views/storage_location/show.blade.php
+++ resources/views/storage_location/show.blade.php
@@ -82,3 +82,11 @@
 </div>
 
 @endsection
+
+@push('script')
+<script>
+     $(document).ready(function(){
+        $('div.overlay').hide();
+    });
+</script>
+@endpush
