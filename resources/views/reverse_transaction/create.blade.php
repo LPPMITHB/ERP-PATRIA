9de0d73da3162fd34c9b5f61f097aac6a9c1fbc9
Benedict Jeremiah <ben.jere0811@gmail.com>
2019-07-25 18:06:58 +0700

    Progress Reverse Transaction (Goods Issue)

--- resources/views/reverse_transaction/create.blade.php
+++ resources/views/reverse_transaction/create.blade.php
@@ -37,12 +37,17 @@
                 <div class="row">
                     <div class="col-sm-4 col-md-4">
                         <div class="box-header no-padding">
-                            <div class="box-body" v:if="documentType == 1">
+                            <div class="box-body" v-if="documentType == 1">
                                 <div class="col-md-4 col-xs-4 no-padding">GR Number</div>
-                                <div class="col-md-6 no-padding">: <b> {{ modelData.number }} </b></div>
+                                <div class="col-md-6 no-padding">: <b> <a class="text-primary" target="_blank" :href="makeShowUrl(modelData.url_type,modelData.id)">{{ modelData.number }}</a></b></div>
                                 
                                 <div class="col-md-4 col-xs-4 no-padding">PO Number</div>
-                                <div class="col-md-6 no-padding">: <b> {{ modelData.purchase_order != null ? modelData.purchase_order.number : '-' }} </b></div>
+                                <div class="col-md-6 no-padding">: 
+                                    <b v-show="modelData.purchase_order != null">
+                                        <a class="text-primary" target="_blank" :href="makeShowUrl(modelData.purchase_order.url_type,modelData.purchase_order_id)">{{ modelData.purchase_order.number }}</a>
+                                    </b>
+                                    <b v-show="modelData.purchase_order == null">-</b>
+                                </div>
 
                                 <div class="col-md-4 col-xs-4 no-padding">Project Number</div>
                                 <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(modelData.purchase_order.project != null ? modelData.purchase_order.project.number : '-')">: <b> {{ modelData.purchase_order.project != null ? modelData.purchase_order.project.number : '-'}} </b></div>
@@ -53,12 +58,35 @@
                                 <div class="col-md-4 col-xs-4 no-padding">Ship Date</div>
                                 <div class="col-md-6 no-padding">: <b> {{ modelData.ship_date != null ? modelData.ship_date.split("-").reverse().join("-") : '-'}} </b></div>
                             </div>
+                            <div class="box-body" v-else-if="documentType == 2">
+                                <div class="col-md-4 col-xs-4 no-padding">GI Number</div>
+                                <div class="col-md-6 no-padding">: <b><a class="text-primary" target="_blank" :href="makeShowUrl(modelData.url_type,modelData.id)">{{ modelData.number }}</a></b></div>
+                                
+                                <div class="col-md-4 col-xs-4 no-padding">MR Number</div>
+                                <div class="col-md-6 no-padding">: 
+                                    <b v-show="modelData.material_requisition != null">
+                                        <a class="text-primary" target="_blank" :href="makeShowUrl(modelData.material_requisition.url_type,modelData.material_requisition_id)">{{ modelData.material_requisition.number }}</a>
+                                    </b>
+                                    <b v-show="modelData.material_requisition == null">-</b>
+                                </div>
+
+                                <div class="col-md-4 col-xs-4 no-padding">Project Number</div>
+                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(modelData.material_requisition.project != null ? modelData.material_requisition.project.number : '-')">: <b> {{ modelData.material_requisition.project != null ? modelData.material_requisition.project.number : '-'}} </b></div>
+
+                                <div class="col-md-4 col-xs-4 no-padding">Issue Date</div>
+                                <div class="col-md-6 no-padding">: <b> {{ modelData.issue_date != null ? modelData.issue_date.split("-").reverse().join("-") : '-'}} </b></div>
+                            </div>
+                            <div v-else></div>
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4">
                         <div class="box-header no-padding">
                             <div class="box-body" v-if="documentType == 1">
                                 <div class="col-md-12 col-xs-12 no-padding">Goods Receipt Description :</div>
+                                <div class="col-md-12 col-xs-12 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(modelData.description)"> <b> {{ modelData.description }} </b></div>
+                            </div>
+                            <div class="box-body" v-else-if="documentType == 2">
+                                <div class="col-md-12 col-xs-12 no-padding">Goods Issue Description :</div>
                                 <div class="col-md-12 col-xs-12 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(modelData.description)"> <b> {{ modelData.description }} </b></div>
                             </div>
                         </div>
@@ -78,7 +106,7 @@
                 <div class="box-body p-t-0">
                     <div class="col-sm-12">
                         <div class="row">
-                            <table id="data-table" class="table table-bordered tableFixed" v-show="documentType == 1">
+                            <table id="data-table" class="table table-bordered tableFixed">
                                 <thead>
                                     <tr>
                                         <th width="5%">No</th>
@@ -86,7 +114,8 @@
                                         <th width="20%">Material Description</th>
                                         <th width="5%">Unit</th>
                                         <th width="25%">Storage Location</th>
-                                        <th width="10%">PO Qty</th>
+                                        <th width="10%" v-if="documentType == 1">PO Qty</th>
+                                        <th width="10%" v-else-if="documentType == 2">MR Qty</th>
                                         <th width="10%">Old Qty</th>
                                         <th width="10%">New Qty</th>
                                     </tr>
@@ -98,9 +127,10 @@
                                         <td class="tdEllipsis" data-container="body" data-toggle="tooltip" :title="(data.material.description)">{{ data.material.description }}</td>
                                         <td class="tdEllipsis">{{ data.material.uom.unit }}</td>
                                         <td class="tdEllipsis">{{ data.storage_location.name }}</td>
-                                        <td class="tdEllipsis">{{ data.po_detail.quantity }}</td>
+                                        <td class="tdEllipsis" v-if="documentType == 1">{{ data.po_detail.quantity }}</td>
+                                        <td class="tdEllipsis" v-else-if="documentType == 2">{{ data.mr_detail.quantity }}</td>
                                         <td class="tdEllipsis">{{ data.quantity }}</td>
-                                        <td class="no-padding"><input class="form-control width100" v-model="data.new_qty" placeholder="New Qty"></td>
+                                        <td class="no-padding"><input :id="index" class="form-control width100" v-model="data.new_qty" placeholder="New Qty"></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -174,6 +204,9 @@
             },
         },
         methods:{
+            makeShowUrl(type,id){
+                return "/"+type+"/"+id;
+            },
             submitForm(){
                 iziToast.question({
                     close: false,
@@ -230,13 +263,40 @@
                     newValue.forEach(data => {
                         if(data.new_qty != ""){
                             var temp = parseFloat((data.new_qty+"").replace(",", ""));
-                            if(temp>data.po_detail.quantity){
-                                iziToast.warning({
-                                    title: 'The new quantity cannot be more than PO quantity',
-                                    position: 'topRight',
-                                    displayMode: 'replace',
+                            if(this.documentType == 1){
+                                if(temp>data.po_detail.quantity){
+                                    iziToast.warning({
+                                        title: 'The new quantity cannot be more than PO quantity',
+                                        position: 'topRight',
+                                        displayMode: 'replace',
+                                    });
+                                    data.new_qty = data.po_detail.quantity;
+                                }
+                            }else if(this.documentType == 2){
+                                var temp_datas = this.modelDataDetails;
+                                temp_datas = JSON.stringify(temp_datas);
+                                temp_datas = JSON.parse(temp_datas);
+
+                                var total_max_qty = 0;
+                                temp_datas.forEach(temp_data => {
+                                    var temp_qty = parseFloat((temp_data.new_qty+"").replace(",", ""));
+                                    if(data.material_id == temp_data.material_id && temp_data.new_qty != ""){
+                                        total_max_qty += temp_qty;
+                                    }
                                 });
-                                data.new_qty = data.po_detail.quantity;
+
+                                if(total_max_qty > data.mr_detail.quantity){
+                                    iziToast.warning({
+                                        title: 'The new quantity for the same material cannot be more than MR quantity',
+                                        position: 'topRight',
+                                        displayMode: 'replace',
+                                    });
+                                    this.modelDataDetails.forEach(remove_qty_data => {
+                                        if(remove_qty_data.material_id == data.material_id){
+                                            remove_qty_data.new_qty = "";
+                                        }
+                                    });
+                                }
                             }
                             if(data.material.uom.is_decimal == 1){
                                 var decimal = (data.new_qty+"").replace(/,/g, '').split('.');
--- resources/views/reverse_transaction/edit.blade.php
+++ resources/views/reverse_transaction/edit.blade.php
@@ -37,29 +37,53 @@
                 <div class="row">
                     <div class="col-sm-4 col-md-4">
                         <div class="box-header no-padding">
-                            <div class="box-body" v:if="modelRT.type == 1">
+                            <div class="box-body" v-if="modelRT.type == 1">
                                 <div class="col-md-4 col-xs-4 no-padding">GR Number</div>
-                                <div class="col-md-6 no-padding">: <b> {{ old_gr_ref.number }} </b></div>
+                                <div class="col-md-6 no-padding">: <b> <a class="text-primary" target="_blank" :href="makeShowUrl(old_data.url_type,old_data.id)">{{ old_data.number }}</a></b></div>
                                 
                                 <div class="col-md-4 col-xs-4 no-padding">PO Number</div>
-                                <div class="col-md-6 no-padding">: <b> {{ old_gr_ref.purchase_order != null ? old_gr_ref.purchase_order.number : '-' }} </b></div>
+                                <div class="col-md-6 no-padding">: 
+                                    <b v-show="old_data.purchase_order != null">
+                                        <a class="text-primary" target="_blank" :href="makeShowUrl(old_data.purchase_order.url_type,old_data.purchase_order_id)">{{ old_data.purchase_order.number }}</a>
+                                    </b>
+                                    <b v-show="old_data.purchase_order == null">-</b>
+                                </div>
 
                                 <div class="col-md-4 col-xs-4 no-padding">Project Number</div>
-                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_gr_ref.purchase_order.project != null ? old_gr_ref.purchase_order.project.number : '-')">: <b> {{ old_gr_ref.purchase_order.project != null ? old_gr_ref.purchase_order.project.number : '-'}} </b></div>
+                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_data.purchase_order.project != null ? old_data.purchase_order.project.number : '-')">: <b> {{ old_data.purchase_order.project != null ? old_data.purchase_order.project.number : '-'}} </b></div>
 
                                 <div class="col-md-4 col-xs-4 no-padding">Vendor Name</div>
-                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_gr_ref.purchase_order != null  ? old_gr_ref.purchase_order.vendor.name : '-')">: <b> {{ old_gr_ref.purchase_order != null  ? old_gr_ref.purchase_order.vendor.name : '-'}} </b></div>
+                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_data.purchase_order != null  ? old_data.purchase_order.vendor.name : '-')">: <b> {{ old_data.purchase_order != null  ? old_data.purchase_order.vendor.name : '-'}} </b></div>
 
                                 <div class="col-md-4 col-xs-4 no-padding">Ship Date</div>
-                                <div class="col-md-6 no-padding">: <b> {{ old_gr_ref.ship_date != null ? old_gr_ref.ship_date.split("-").reverse().join("-") : '-'}} </b></div>
+                                <div class="col-md-6 no-padding">: <b> {{ old_data.ship_date != null ? old_data.ship_date.split("-").reverse().join("-") : '-'}} </b></div>
                             </div>
+                            <div class="box-body" v-else-if="modelRT.type == 2">
+                                <div class="col-md-4 col-xs-4 no-padding">GI Number</div>
+                                <div class="col-md-6 no-padding">: <b><a class="text-primary" target="_blank" :href="makeShowUrl(old_data.url_type,old_data.id)">{{ old_data.number }}</a></b></div>
+                                
+                                <div class="col-md-4 col-xs-4 no-padding">MR Number</div>
+                                <div class="col-md-6 no-padding">: 
+                                    <b v-show="old_data.material_requisition != null">
+                                        <a class="text-primary" target="_blank" :href="makeShowUrl(old_data.material_requisition.url_type,old_data.material_requisition_id)">{{ old_data.material_requisition.number }}</a>
+                                    </b>
+                                    <b v-show="old_data.material_requisition == null">-</b>
+                                </div>
+
+                                <div class="col-md-4 col-xs-4 no-padding">Project Number</div>
+                                <div class="col-md-6 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_data.material_requisition.project != null ? old_data.material_requisition.project.number : '-')">: <b> {{ old_data.material_requisition.project != null ? old_data.material_requisition.project.number : '-'}} </b></div>
+
+                                <div class="col-md-4 col-xs-4 no-padding">Issue Date</div>
+                                <div class="col-md-6 no-padding">: <b> {{ old_data.issue_date != null ? old_data.issue_date.split("-").reverse().join("-") : '-'}} </b></div>
+                            </div>
+                            <div v-else></div>
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4">
                         <div class="box-header no-padding">
                             <div class="box-body" v-if="modelRT.type == 1">
                                 <div class="col-md-12 col-xs-12 no-padding">Goods Receipt Description :</div>
-                                <div class="col-md-12 col-xs-12 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_gr_ref.description)"> <b> {{ old_gr_ref.description }} </b></div>
+                                <div class="col-md-12 col-xs-12 no-padding tdEllipsis" data-container="body" data-toggle="tooltip" :title="(old_data.description)"> <b> {{ old_data.description }} </b></div>
                             </div>
                         </div>
                     </div>
@@ -78,7 +102,7 @@
                 <div class="box-body p-t-0">
                     <div class="col-sm-12">
                         <div class="row">
-                            <table id="data-table" class="table table-bordered tableFixed" v-show="modelRT.type == 1">
+                            <table id="data-table" class="table table-bordered tableFixed">
                                 <thead>
                                     <tr>
                                         <th width="5%">No</th>
@@ -86,7 +110,8 @@
                                         <th width="20%">Material Description</th>
                                         <th width="5%">Unit</th>
                                         <th width="25%">Storage Location</th>
-                                        <th width="10%">PO Qty</th>
+                                        <th width="10%" v-if="modelRT.type == 1">PO Qty</th>
+                                        <th width="10%" v-else-if="modelRT.type == 2">MR Qty</th>
                                         <th width="10%">Old Qty</th>
                                         <th width="10%">New Qty</th>
                                     </tr>
@@ -97,10 +122,12 @@
                                         <td class="tdEllipsis">{{ data.material.code }}</td>
                                         <td class="tdEllipsis" data-container="body" data-toggle="tooltip" :title="(data.material.description)">{{ data.material.description }}</td>
                                         <td class="tdEllipsis">{{ data.material.uom.unit }}</td>
-                                        <td class="tdEllipsis">{{ data.grd.storage_location.name }}</td>
-                                        <td class="tdEllipsis">{{ data.po_detail.quantity }}</td>
+                                        <td class="tdEllipsis" v-if="modelRT.type == 1">{{ data.grd.storage_location.name }}</td>
+                                        <td class="tdEllipsis" v-if="modelRT.type == 2">{{ data.gid.storage_location.name }}</td>
+                                        <td class="tdEllipsis" v-if="modelRT.type == 1">{{ data.po_detail.quantity }}</td>
+                                        <td class="tdEllipsis" v-else-if="modelRT.type == 2">{{ data.mr_detail.quantity }}</td>
                                         <td class="tdEllipsis">{{ data.old_quantity }}</td>
-                                        <td class="no-padding"><input class="form-control width100" v-model="data.new_quantity" placeholder="New Qty"></td>
+                                        <td class="no-padding"><input :id="index" class="form-control width100" v-model="data.new_quantity" placeholder="New Qty"></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -150,7 +177,7 @@
     var data = {
         modelRTDetails : @json($modelRTDetails),
         modelRT : @json($modelRT),
-        old_gr_ref : @json($old_gr_ref),
+        old_data : @json($old_data),
         status : @json($status),
         menu : @json($menu),
         description : @json($modelRT->description),
@@ -177,6 +204,9 @@
             },
         },
         methods:{
+            makeShowUrl(type,id){
+                return "/"+type+"/"+id;
+            },
             submitForm(){
                 iziToast.question({
                     close: false,
@@ -231,13 +261,40 @@
                     newValue.forEach(data => {
                         if(data.new_quantity != ""){
                             var temp = parseFloat((data.new_quantity+"").replace(",", ""));
-                            if(temp>data.po_detail.quantity){
-                                iziToast.warning({
-                                    title: 'The new quantity cannot be more than PO quantity',
-                                    position: 'topRight',
-                                    displayMode: 'replace',
+                            if(this.modelRT.type == 1){
+                                if(temp>data.po_detail.quantity){
+                                    iziToast.warning({
+                                        title: 'The new quantity cannot be more than PO quantity',
+                                        position: 'topRight',
+                                        displayMode: 'replace',
+                                    });
+                                    data.new_quantity = data.po_detail.quantity;
+                                }
+                            }else if(this.modelRT.type == 2){
+                                var temp_datas = this.modelRTDetails;
+                                temp_datas = JSON.stringify(temp_datas);
+                                temp_datas = JSON.parse(temp_datas);
+
+                                var total_max_qty = 0;
+                                temp_datas.forEach(temp_data => {
+                                    var temp_qty = parseFloat((temp_data.new_quantity+"").replace(",", ""));
+                                    if(data.material_id == temp_data.material_id && temp_data.new_quantity != ""){
+                                        total_max_qty += temp_qty;
+                                    }
                                 });
-                                data.new_quantity = data.po_detail.quantity;
+
+                                if(total_max_qty > data.mr_detail.quantity){
+                                    iziToast.warning({
+                                        title: 'The new quantity for the same material cannot be more than MR quantity',
+                                        position: 'topRight',
+                                        displayMode: 'replace',
+                                    });
+                                    this.modelRTDetails.forEach(remove_qty_data => {
+                                        if(remove_qty_data.material_id == data.material_id){
+                                            remove_qty_data.new_quantity = "";
+                                        }
+                                    });
+                                }
                             }
                             if(data.material.uom.is_decimal == 1){
                                 var decimal = (data.new_quantity+"").replace(/,/g, '').split('.');
