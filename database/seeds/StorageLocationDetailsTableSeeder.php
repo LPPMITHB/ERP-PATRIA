12e7c35d83563d5a555b32bb1222f03d24286c1c
mynameyoyoo <mynameyoyoo@gmail.com>
2019-07-29 14:26:52 +0700

    sloc detail seeder

--- database/seeds/StorageLocationDetailsTableSeeder.php
+++ database/seeds/StorageLocationDetailsTableSeeder.php
@@ -1,33 +1,233 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\Material; 
 
 class StorageLocationDetailsTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        $modelMaterials = Material::all();
-        foreach($modelMaterials as $material){
-            $quantity = rand(1,1000);
-            DB::table('mst_storage_location_detail')->insert([
-                'material_id' => $material->id,
-                'quantity' => $quantity,
-                'storage_location_id' => rand(1,3),
-            ]);
+        
 
-            $quantity = rand(1,1000);
-            DB::table('mst_storage_location_detail')->insert([
-                'material_id' => $material->id,
-                'quantity' => $quantity,
-                'storage_location_id' => rand(4,7),
-            ]);
-
-        }
+        \DB::table('mst_storage_location_detail')->delete();
+        
+        \DB::table('mst_storage_location_detail')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'material_id' => 1780,
+                'quantity' => 15.5,
+                'value' => 100000.0,
+                'goods_receipt_detail_id' => 1,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'material_id' => 293,
+                'quantity' => 25.0,
+                'value' => 250000.0,
+                'goods_receipt_detail_id' => 2,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'material_id' => 10,
+                'quantity' => 5.0,
+                'value' => 500000.0,
+                'goods_receipt_detail_id' => 3,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'material_id' => 316,
+                'quantity' => 7.0,
+                'value' => 55000.0,
+                'goods_receipt_detail_id' => 4,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'material_id' => 1130,
+                'quantity' => 3.0,
+                'value' => 25000.0,
+                'goods_receipt_detail_id' => 5,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'material_id' => 1070,
+                'quantity' => 8.0,
+                'value' => 75000.0,
+                'goods_receipt_detail_id' => 6,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'material_id' => 1220,
+                'quantity' => 2.0,
+                'value' => 1250000.0,
+                'goods_receipt_detail_id' => 7,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:10:18',
+                'updated_at' => '2019-07-29 14:10:18',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'material_id' => 5,
+                'quantity' => 23.0,
+                'value' => 10000.0,
+                'goods_receipt_detail_id' => 8,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:19:49',
+                'updated_at' => '2019-07-29 14:19:49',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'material_id' => 4,
+                'quantity' => 15.0,
+                'value' => 1000000.0,
+                'goods_receipt_detail_id' => 9,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:19:49',
+                'updated_at' => '2019-07-29 14:19:49',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'material_id' => 1,
+                'quantity' => 8.0,
+                'value' => 200000.0,
+                'goods_receipt_detail_id' => 10,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:19:49',
+                'updated_at' => '2019-07-29 14:19:49',
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'material_id' => 7,
+                'quantity' => 12.0,
+                'value' => 500000.0,
+                'goods_receipt_detail_id' => 11,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:19:49',
+                'updated_at' => '2019-07-29 14:19:49',
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'material_id' => 25,
+                'quantity' => 3.0,
+                'value' => 500000.0,
+                'goods_receipt_detail_id' => 12,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:19:49',
+                'updated_at' => '2019-07-29 14:19:49',
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'material_id' => 4,
+                'quantity' => 12.0,
+                'value' => 500000.0,
+                'goods_receipt_detail_id' => 13,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:11',
+                'updated_at' => '2019-07-29 14:22:11',
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'material_id' => 13,
+                'quantity' => 5.0,
+                'value' => 500000.0,
+                'goods_receipt_detail_id' => 14,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:11',
+                'updated_at' => '2019-07-29 14:22:11',
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'material_id' => 28,
+                'quantity' => 15.0,
+                'value' => 250000.0,
+                'goods_receipt_detail_id' => 15,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:11',
+                'updated_at' => '2019-07-29 14:22:11',
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'material_id' => 32,
+                'quantity' => 5.0,
+                'value' => 55000.0,
+                'goods_receipt_detail_id' => 16,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:11',
+                'updated_at' => '2019-07-29 14:22:11',
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'material_id' => 38,
+                'quantity' => 8.0,
+                'value' => 250000.0,
+                'goods_receipt_detail_id' => 17,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:11',
+                'updated_at' => '2019-07-29 14:22:11',
+            ),
+            17 => 
+            array (
+                'id' => 18,
+                'material_id' => 1780,
+                'quantity' => 30.0,
+                'value' => 75000.0,
+                'goods_receipt_detail_id' => 18,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:23',
+                'updated_at' => '2019-07-29 14:22:23',
+            ),
+            18 => 
+            array (
+                'id' => 19,
+                'material_id' => 1781,
+                'quantity' => 40.0,
+                'value' => 100000.0,
+                'goods_receipt_detail_id' => 19,
+                'storage_location_id' => 1,
+                'created_at' => '2019-07-29 14:22:23',
+                'updated_at' => '2019-07-29 14:22:23',
+            ),
+        ));
+        
+        
     }
 }
