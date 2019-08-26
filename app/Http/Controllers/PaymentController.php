34be2cbc71673b7909af511bf92f0d3145a8fa89
mynameyoyoo <mynameyoyoo@gmail.com>
2019-08-26 15:01:56 +0700

    manage payment done

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use App\Models\Payment;
+use DB;
+use Auth;
 
 class PaymentController extends Controller
 {
@@ -12,6 +17,22 @@ class PaymentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function selectInvoice(Request $request){
+        $route = $request->route()->getPrefix();
+        $modelInvoice = Invoice::whereIn('status', [1,3])->get();
+        
+        return view('payment.select_invoice', compact('modelInvoice', 'route'));
+    }
+
+    public function manage(Request $request, $id)
+    {
+        $route = $request->route()->getPrefix();
+        $payment = Payment::where('invoice_id',$id)->with('user')->get();
+        $invoice = Invoice::where('id', $id)->with('salesOrder','project','project.ship','project.customer')->first();
+
+        return view('payment.manage', compact('invoice','route','payment'));
+    }
+
     public function index()
     {
         //
@@ -35,7 +56,38 @@ class PaymentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->json()->all();
+
+        DB::beginTransaction();
+        try {
+            $payment = new Payment;
+            $payment->invoice_id = $data['invoice_id'];
+            $payment->paid_value = $data['amount'];
+            $payment->status = 1;
+            $payment->user_id = Auth::user()->id;
+            $payment->branch_id = Auth::user()->branch->id;
+            $payment->save();
+
+            $modelInvoice = Invoice::findOrFail($data['invoice_id']);
+            $modelPayment = Payment::where('invoice_id',$data['invoice_id'])->get();
+            
+            $paid = 0;
+            foreach($modelPayment as $payment){
+                $paid += $payment->paid_value;
+            }
+            if($paid < $modelInvoice->payment_value){
+                $modelInvoice->status = 3;
+            }else{
+                $modelInvoice->status = 0;
+            }
+
+            $modelInvoice->update();
+            DB::commit();
+            return response(json_encode($data),Response::HTTP_OK);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->route('payment.selectInvoice')->with('error', $e->getMessage());
+        }
     }
 
     /**
@@ -81,5 +133,18 @@ class PaymentController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    // function
+    public function getInvoiceAPI($id){
+        $invoice = Invoice::where('id', $id)->with('salesOrder','project','project.ship','project.customer')->first()->jsonSerialize();
+
+        return response($invoice, Response::HTTP_OK);
+    }
+
+    public function getPaymentAPI($id){
+        $payment = Payment::where('invoice_id',$id)->with('user')->get()->jsonSerialize();
+
+        return response($payment, Response::HTTP_OK);
     }
 }
