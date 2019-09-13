9933faab134541f1a22fabc32d94f8c9a2303b00
Benedict Jeremiah <ben.jere0811@gmail.com>
2019-09-13 10:03:11 +0700

    bf

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1423,8 +1423,7 @@ class ProjectController extends Controller
             $project->description = $request->description;
             $project->customer_id = $request->customer;
             $project->ship_id = $request->ship;
-            $project->budget_value = $request->budget_value_int;
-            
+            $project->budget_value = str_replace(",", "", $request->budget_value);
             $project->flag = $request->flag;
             $project->class_name = $request->class_name;
             $project->person_in_charge = $request->person_in_charge;
@@ -1551,7 +1550,7 @@ class ProjectController extends Controller
             $plannedCostPerWbs = $wbs->bom != null ? $wbs->bom->rap != null ? $wbs->bom->rap->total_price : 0 : 0;
 
             foreach($wbs->materialRequisitionDetails as $mrd){
-                $actualCostPerWbs = $mrd->material->cost_standard_price * $mrd->issued;
+                $actualCostPerWbs += $mrd->material->cost_standard_price * $mrd->issued;
             }
 
             $planned->put($wbs->id,[
