33971ed1e25e55f93ac754cd572e199dedbf255f
Benedict Jeremiah <ben.jere0811@gmail.com>
2019-09-13 09:26:33 +0700

    Bug FIx

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1423,7 +1423,7 @@ class ProjectController extends Controller
             $project->description = $request->description;
             $project->customer_id = $request->customer;
             $project->ship_id = $request->ship;
-            $project->budget_value = $request->budget_value;
+            $project->budget_value = $request->budget_value_int;
             $project->flag = $request->flag;
             $project->class_name = $request->class_name;
             $project->person_in_charge = $request->person_in_charge;
