a171d0a3fe471048994e02f59a5d44cb9d6b64a2
Rei PK <Reinardy@Gmail.com>
2019-10-06 22:44:36 +0700

    Progress Repeat Order 3 (JGN MERGE)

--- resources/views/purchase_requisition/repeatOrder.blade.php
+++ resources/views/purchase_requisition/repeatOrder.blade.php
@@ -18,90 +18,108 @@
     <div class="col-xs-12">
         <div class="box">
             <div class="box-body">
-                <form id="" class="form-horizontal" method="POST" action="{{ route('material_requisition_repair.store') }}">
-                @csrf
-                @verbatim
-                    <div id="pr">
-                        <div class="box-header no-padding">
-                            <div class="col-xs-12 col-md-4">
-                                <label>Select Existing Purchase Requisition to Repeat</label>
-                                <selectize v-model="selectedPR_id">
-                                    <option v-for="(modelPR, index) in modelPRs" :value="modelPR.id">{{ modelPR.number }} - {{ modelPR.description }}</option>
-                                </selectize>
-                            </div>
-                        </div>
-                        <div v-if="selectedPR.length > 0" >
-                            <div class="box-body no-padding">
-                                <span class="info-box-text">PR Number</span>
-                                <span class="info-box-number">{{ selectedPR.length > 0 ? selectedPR[0].number : "-"}}</span>
-                            </div>
-                            <div class="col-sm-4 col-md-4 m-t-10 m-l-10">
-                                <div class="row" v-if="selectedPR.length > 0">
-                                    <div class="col-xs-4 col-md-4">
-                                        Created By
-                                    </div>
-                                    <div class="col-xs-8 col-md-8">
-                                        : <b>{{ selectedPR[0].user.name }}</b>
-                                    </div>
-                                    <div class="col-xs-4 col-md-4">
-                                        Created At
-                                    </div>
-                                    <div class="col-xs-8 col-md-8">
-                                        : <b>{{ selectedPR[0].new_created }}</b>
-                                    </div>
-                                    <div class="col-xs-4 col-md-4">
-                                        Description
-                                    </div>
-                                    <div class="col-xs-8 col-md-8 tdEllipsis" data-container="body" data-toggle="tooltip" title="">
-                                        : <b>{{ selectedPR[0].description }}</b>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <table class="table table-bordered tableFixed showTable" id="pr-table">
-                                    <thead>
-                                        <tr v-if="selectedPR[0].type == 3">
-                                            <th width="5%">No</th>
-                                            <th width="15%">Project Number</th>
-                                            <th width="25%">WBS</th>
-                                            <th width="40%">Job Order</th>
-                                        </tr>
-                                        <tr v-else-if="selectedPR[0].type == 1">
-                                            <th width="5%">No</th>
-                                            <th width="75%">Material</th>
-                                            <th width="12%">Request Quantity</th>
-                                            <th width="8%">Unit</th>
-                                        </tr>
-                                        <tr v-else>
-                                            <th width="20%">Resource Number</th>
-                                            <th width="25%">Resource Description</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <template v-for="(selectedPRD,index) in selectedPR[0].purchase_requisition_details" v-if="selectedPR[0].type == 3">
-                                            <tr>
-                                                <td>{{ index + 1 }}</td>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                            </tr>
-                                        </template>
-                                        <template v-for="(selectedPRD,index) in selectedPR[0].purchase_requisition_details" v-if="selectedPR[0].type != 3">
-                                            <tr>
-                                                <td>{{ index + 1 }}</td>
-                                                <td>{{ selectedPRD.material.code }} - {{ selectedPRD.material.description }}</td>
-                                                <td>{{ selectedPRD.quantity }}</td>
-                                                <td>{{ selectedPRD.material.uom.unit }}</td>
-                                            </tr>
-                                        </template>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <a class="col-xs-12 col-md-2 btn btn-primary pull-right m-r-5" onclick="">CREATE</a>
+            @csrf
+            @verbatim
+                <div id="pr">
+                    <div class="box-header no-padding">
+                        <div class="col-xs-12 col-md-4">
+                            <label>Select Existing Purchase Requisition to Repeat</label>
+                            <selectize v-model="selectedPR_id">
+                                <option v-for="(modelPR, index) in modelPRs" :value="modelPR.id">{{ modelPR.number }} - {{ modelPR.description }}</option>
+                            </selectize>
                         </div>
                     </div>
-                @endverbatim
-                </form>
+                    <div v-if="selectedPR.length > 0" >
+                        <div class="box-body no-padding">
+                            <span class="info-box-text">PR Number</span>
+                            <span class="info-box-number">{{ selectedPR.length > 0 ? selectedPR[0].number : "-"}}</span>
+                        </div>
+                        <div class="col-sm-4 col-md-4 m-t-10 m-l-10">
+                            <div class="row" v-if="selectedPR.length > 0">
+                                <div class="col-xs-4 col-md-4">
+                                    Created By
+                                </div>
+                                <div class="col-xs-8 col-md-8">
+                                    : <b>{{ selectedPR[0].user.name }}</b>
+                                </div>
+                                <div class="col-xs-4 col-md-4">
+                                    Created At
+                                </div>
+                                <div class="col-xs-8 col-md-8">
+                                    : <b>{{ selectedPR[0].new_created }}</b>
+                                </div>
+                                <div class="col-xs-4 col-md-4">
+                                    Description
+                                </div>
+                                <div class="col-xs-8 col-md-8 tdEllipsis" data-container="body" data-toggle="tooltip" title="">
+                                    : <b>{{ selectedPR[0].description }}</b>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <table class="table table-bordered tableFixed showTable" id="pr-table">
+                                <thead>
+                                    <tr v-if="selectedPR[0].type == 3">
+                                        <th width="4%">No</th>
+                                        <th width="76%">Project Number</th>
+                                        <th width="12%">WBS</th>
+                                        <th width="8%">Job Order</th>
+                                    </tr>
+                                    <tr v-else-if="selectedPR[0].type == 1">
+                                        <th width="4%">No</th>
+                                        <th width="76%">Material</th>
+                                        <th width="12%">Request Quantity</th>
+                                        <th width="8%">Unit</th>
+                                    </tr>
+                                    <tr v-else>
+                                        <th width="4%">No</th>
+                                        <th width="41%">Resource Number</th>
+                                        <th width="55%">Resource Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <template v-for="(selectedPRD,index) in selectedPR[0].purchase_requisition_details" v-if="selectedPR[0].type == 3">
+                                        <tr>
+                                            <td>{{ index + 1 }}</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </template>
+                                    <template v-for="(selectedPRD,index) in selectedPR[0].purchase_requisition_details" v-if="selectedPR[0].type != 3">
+                                        <tr>
+                                            <td>{{ index + 1 }}</td>
+                                            <td>{{ selectedPRD.material.code }} - {{ selectedPRD.material.description }}</td>
+                                            <td>{{ selectedPRD.quantity }}</td>
+                                            <td>{{ selectedPRD.material.uom.unit }}</td>
+                                        </tr>
+                                    </template>
+                                </tbody>
+                            </table>
+                        </div>
+    					<a class="col-xs-12 col-md-2 btn btn-primary pull-right m-r-5" onclick="">CREATE</a>
+						<div class="modal fade" id="create-repeat-order">
+							<div class="modal-dialog modalFull">
+								<form>
+				                    <div class="modal-content">
+				                        <div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				                                <span aria-hidden="true">×</span>
+				                            </button>
+				                            <h4 class="modal-title">Create Repeat Order</h4>
+										</div>
+										<div class="modal-body">
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-primary" :disabled="updateOk" data-dismiss="modal" @click.prevent="create">CREATE</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+                    </div>
+                </div>
+            @endverbatim
             </div>
             <div class="overlay">
                 <i class="fa fa-refresh fa-spin"></i>
@@ -123,7 +141,6 @@
         modelPRs : @json($modelPRs),
         dataInput : {
             pr_id :"",
-
         },
     }
 
@@ -131,7 +148,10 @@
         el : '#pr',
         data : data,
         methods : {
+			create(){
 
+		        form.submit();
+		    },
         },
         watch : {
             'selectedPR_id' : function(newValue){
