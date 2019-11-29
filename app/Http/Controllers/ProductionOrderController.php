dbd5513fef23a1cd581c0e73fa315d2981a97ba5
Benedict Jeremiah <ben.jere0811@gmail.com>
2019-11-29 16:26:37 +0700

    bug fixing

--- app/Http/Controllers/ProductionOrderController.php
+++ app/Http/Controllers/ProductionOrderController.php
@@ -1246,85 +1246,85 @@ class ProductionOrderController extends Controller
     public function storeConfirmRepair(Request $request){
         $route = $request->route()->getPrefix();
         $datas = json_decode($request->datas);
-        if(count($datas->modelPrOD)==0){
-            $wbs = $datas->modelPrO->wbs;
-            $wbs = WBS::find($wbs->id);
+        // if(count($datas->modelPrOD)==0){
+        //     $wbs = $datas->modelPrO->wbs;
+        //     $wbs = WBS::find($wbs->id);
 
-            $statusAll = $wbs->activities->groupBy('status');
-            $notDone = true;
-            foreach($statusAll as $key => $status){
-                if($key == 1){
-                    $notDone = false;
-                }
-            }
-            $modelPrO = ProductionOrder::find($datas->modelPrO->id);
-            if($notDone){
-                $modelPrO->status = 0;
-                $modelPrO->save();
-            }else{
-                $modelPrO->status = 2;
-                $modelPrO->save();
-            }
+        //     $statusAll = $wbs->activities->groupBy('status');
+        //     $notDone = true;
+        //     foreach($statusAll as $key => $status){
+        //         if($key == 1){
+        //             $notDone = false;
+        //         }
+        //     }
+        //     $modelPrO = ProductionOrder::find($datas->modelPrO->id);
+        //     if($notDone){
+        //         $modelPrO->status = 0;
+        //         $modelPrO->save();
+        //     }else{
+        //         $modelPrO->status = 2;
+        //         $modelPrO->save();
+        //     }
 
-            if($route == "/production_order"){
-                return redirect()->route('production_order.showConfirm',$datas->modelPrO->id)->with('success', 'Production Order Confirmed');
-            }elseif($route == "/production_order_repair"){
-                return redirect()->route('production_order_repair.showConfirm',$datas->modelPrO->id)->with('success', 'Production Order Confirmed');
-            }
-        }
+        //     if($route == "/production_order"){
+        //         return redirect()->route('production_order.showConfirm',$datas->modelPrO->id)->with('success', 'Production Order Confirmed');
+        //     }elseif($route == "/production_order_repair"){
+        //         return redirect()->route('production_order_repair.showConfirm',$datas->modelPrO->id)->with('success', 'Production Order Confirmed');
+        //     }
+        // }
         $pro_id = $datas->modelPrOD[0]->production_order_id;
         $modelPrO = ProductionOrder::findOrFail($pro_id);
 
         DB::beginTransaction();
         try {
-            $statusAll = $modelPrO->wbs->activities->groupBy('status');
-            $notDone = true;
-            foreach($statusAll as $key => $status){
-                if($key == 1){
-                    $notDone = false;
-                }
-            }
-            if($notDone){
-                $modelPrO->status = 0;
-                $modelPrO->save();
-            }else{
-                $modelPrO->status = 2;
-                $modelPrO->save();
-            }
+            // $statusAll = $modelPrO->wbs->activities->groupBy('status');
+            // $notDone = true;
+            // foreach($statusAll as $key => $status){
+            //     if($key == 1){
+            //         $notDone = false;
+            //     }
+            // }
+            // if($notDone){
+            //     $modelPrO->status = 0;
+            //     $modelPrO->save();
+            // }else{
+            //     $modelPrO->status = 2;
+            //     $modelPrO->save();
+            // }
             if($datas->data_changed){
                 foreach ($datas->materials as $material) {
                     $prod = ProductionOrderDetail::find($material->id);
-                    if($material->dimension_uom_id != null){
-                        if($prod->length == null){
-                            if($prod->actual != null){
-                                $prod->actual += $material->quantity > 0 ? $material->quantity : 0;
-                            }else{
-                                $prod->actual = $material->quantity > 0 ? $material->quantity : null;
-                            }
-                            $prod->update();
-                        }
-                        if($material->weight > 0){
-                            $PrOD = new ProductionOrderDetail;
-                            $PrOD->production_order_id = $modelPrO->id;
-                            $PrOD->production_order_detail_id = $prod->id;
-                            $PrOD->material_id = $material->id;
-                            $PrOD->length = $material->lengths;
-                            $PrOD->width = $material->width;
-                            $PrOD->height = $material->height;
-                            $PrOD->weight = $material->weight;
-                            $PrOD->dimension_uom_id = $prod->dimension_uom_id;
-                            $PrOD->quantity = $material->quantity;
-                            $PrOD->source = 'Stock';
-                            $PrOD->save();
-                        }
-                    }else{
-                        if($prod->actual != null){
-                            $prod->actual += $material->quantity > 0 ? $material->quantity : 0;
-                        }else{
-                            $prod->actual = $material->quantity > 0 ? $material->quantity : null;
-                        }
-                        $prod->update();
-                    }
+                    // if($material->dimension_uom_id != null){
+                    //     if($prod->length == null){
+                    //         if($prod->actual != null){
+                    //             $prod->actual += $material->quantity > 0 ? $material->quantity : 0;
+                    //         }else{
+                    //             $prod->actual = $material->quantity > 0 ? $material->quantity : null;
+                    //         }
+                    //         $prod->update();
+                    //     }
+                    //     if($material->weight > 0){
+                    //         $PrOD = new ProductionOrderDetail;
+                    //         $PrOD->production_order_id = $modelPrO->id;
+                    //         $PrOD->production_order_detail_id = $prod->id;
+                    //         $PrOD->material_id = $material->id;
+                    //         $PrOD->length = $material->lengths;
+                    //         $PrOD->width = $material->width;
+                    //         $PrOD->height = $material->height;
+                    //         $PrOD->weight = $material->weight;
+                    //         $PrOD->dimension_uom_id = $prod->dimension_uom_id;
+                    //         $PrOD->quantity = $material->quantity;
+                    //         $PrOD->source = 'Stock';
+                    //         $PrOD->save();
+                    //     }
+                    // }else{
+                    //     if($prod->actual != null){
+                    //         $prod->actual += $material->quantity > 0 ? $material->quantity : 0;
+                    //     }else{
+                    //         $prod->actual = $material->quantity > 0 ? $material->quantity : null;
+                    //     }
+                    //     $prod->update();
+                    // }
                     
                     
     
