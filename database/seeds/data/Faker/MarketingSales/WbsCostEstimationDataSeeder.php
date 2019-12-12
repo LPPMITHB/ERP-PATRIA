7fac0d00138a72d73ec993d65895a39c781cff13
xtabs12 <ccfc2015jupri@gmail.com>
2019-12-12 16:47:14 +0700

    managing database

---
+++ database/seeds/data/Faker/MarketingSales/WbsCostEstimationDataSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Data;
+
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.0.1
+ */
+
+/**
+ * Database `erp_patria`
+ */
+class WbsCostEstimationDataSeeder
+{
+    /* `erp_patria`.`menus` */
+    public static function getData()
+    {
+        return array(
+            
+        );
+    }
+}
